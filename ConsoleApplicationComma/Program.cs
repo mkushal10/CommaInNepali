3009639fc15ba8d52effdafead3c4d3143bbc7f3
Kushal Neupane <mkushal10@gmail.com>
2019-07-02 17:40:07 +0545

    Add project files.

---
+++ ConsoleApplicationComma/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApplicationComma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = "starts";
+            while (start == "starts")
+            {
+                int nums;
+                bool result = false;
+                while (result == false)
+                {
+                    Console.WriteLine("Please Enter a Number: ");
+                    string input = Console.ReadLine();
+                    result = int.TryParse(input, out nums);
+                    int len = input.Length;
+                    if (result == false)
+                    {
+                        Console.Write("\n Please Enter Numbers Only.");
+                        break;
+                    }
+                    else
+                    {
+                        if (len > 3)
+                        {
+                            int lens = len - 3;
+                            Console.WriteLine(lens);
+                            if (lens % 2 == 0)
+                            {
+                                for (int i =0; i<=lens; i+=2)
+                                {
+                                    Console.WriteLine(i);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Hello 0");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(input);
+                        }
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+//string ctrs = ",";
+//Console.WriteLine(input + ctrs);
